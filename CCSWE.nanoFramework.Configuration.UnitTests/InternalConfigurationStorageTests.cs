d5c305e48ed2425e57aeb389c4c40a3b2ba5f82a
Cory Charlton <ccharlton@pobox.com>
2023-11-16 09:22:34 -0800

    Added InternalConfigurationStorage tests

---
+++ CCSWE.nanoFramework.Configuration.UnitTests/InternalConfigurationStorageTests.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using CCSWE.nanoFramework.Configuration.UnitTests.Mocks;
+using nanoFramework.Json;
+using nanoFramework.TestFramework;
+
+namespace CCSWE.nanoFramework.Configuration.UnitTests
+{
+    [TestClass]
+    public class InternalConfigurationStorageTests
+    {
+        [TestMethod]
+        public void DeleteConfiguration_deletes_configuration_file()
+        {
+            var path = InternalConfigurationStorage.GetPath(ConfigurationMock.Section);
+            var sut = new InternalConfigurationStorage();
+
+            try
+            {
+                File.Create(path).Dispose();
+
+                sut.DeleteConfiguration(ConfigurationMock.Section);
+
+                Assert.IsFalse(File.Exists(path), "File.Exists(path)");
+            }
+            finally
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ReadConfiguration_returns_null_if_configuration_does_not_exist()
+        {
+            var sut = new InternalConfigurationStorage();
+
+            Assert.IsNull(sut.ReadConfiguration(ConfigurationMock.Section, typeof(ConfigurationMock)), "Configuration is null");
+        }
+
+        [TestMethod]
+        public void ReadConfiguration_throws_if_configuration_cannot_be_deserialized()
+        {
+            var path = InternalConfigurationStorage.GetPath(ConfigurationMock.Section);
+            var sut = new InternalConfigurationStorage();
+
+            try
+            {
+                File.WriteAllText(path, "Invalid JSON content");
+
+                Assert.ThrowsException(typeof(DeserializationException), () =>
+                {
+                    sut.ReadConfiguration(ConfigurationMock.Section, typeof(ConfigurationMock));
+                });
+            }
+            finally
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void WriteConfiguration_can_be_read_by_ReadConfiguration()
+        {
+            var expected = ConfigurationMock.Create();
+            var path = InternalConfigurationStorage.GetPath(ConfigurationMock.Section);
+            var sut = new InternalConfigurationStorage();
+
+            try
+            {
+                sut.WriteConfiguration(ConfigurationMock.Section, expected);
+                Assert.IsTrue(File.Exists(path), "File.Exists(path)");
+
+                var actual = sut.ReadConfiguration(ConfigurationMock.Section, typeof(ConfigurationMock));
+
+                Assert.AreEqual(expected, actual);
+            }
+            finally
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+    }
+}
---
+++ CCSWE.nanoFramework.Configuration.UnitTests/Mocks/ConfigurationMock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CCSWE.nanoFramework.Configuration.UnitTests.Mocks
+{
+    internal class ConfigurationMock
+    {
+        public const string Section = nameof(ConfigurationMock);
+
+        public ConfigurationMock()
+        {
+
+        }
+
+        public ConfigurationMock(
+            bool boolSetting,
+            DateTime dateTimeSetting,
+            double doubleSetting,
+            float floatSetting,
+            int intSetting,
+            string stringSetting,
+            TimeSpan timeSpanSetting)
+        {
+            BoolSetting = boolSetting;
+            DateTimeSetting = dateTimeSetting;
+            DoubleSetting = doubleSetting;
+            FloatSetting = floatSetting;
+            IntSetting = intSetting;
+            StringSetting = stringSetting;
+            TimeSpanSetting = timeSpanSetting;
+        }
+
+        public bool BoolSetting { get; set; }
+        public DateTime DateTimeSetting { get; set; }
+        public double DoubleSetting { get; set; }
+        public float FloatSetting { get; set; }
+        public int IntSetting { get; set; }
+        public string StringSetting { get; set; }
+        public TimeSpan TimeSpanSetting { get; set; }
+
+        public static ConfigurationMock Create()
+        {
+            return new ConfigurationMock(true, DateTime.UtcNow, 0.1234d, 0.4321f, 1234, "A String", TimeSpan.FromMinutes(1234));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ConfigurationMock);
+        }
+
+        public bool Equals(ConfigurationMock other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return BoolSetting == other.BoolSetting && 
+                   DateTimeSetting == other.DateTimeSetting &&
+                   Math.Abs(DoubleSetting - other.DoubleSetting) < double.Epsilon &&
+                   Math.Abs(FloatSetting - other.FloatSetting) < float.Epsilon &&
+                   IntSetting == other.IntSetting &&
+                   StringSetting == other.StringSetting &&
+                   TimeSpanSetting == other.TimeSpanSetting;
+        }
+    }
+}
